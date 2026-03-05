ea28c5b1a0cd08b310437545a784c6a86d5d338c
Vegard <vegard.valvik@gmail.com>
2026-03-05 21:03:05 +0900

    test redirect

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/linni",
+        destination: "https://nrk.no",
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
